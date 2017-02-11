1e3594790f16245a65d2a5319a263a206b2a19ed
prajankya <prajankya@gmail.com>
2017-02-11 19:22:07 +0530

    added module export feature

--- index.js
+++ index.js
@@ -2,25 +2,42 @@ const fs = require('fs')
 const path = require('path')
 const debug = require('debug')('NodeRed_Container:getRedFile');
 
-var RED
+var getRED = function (dir, cb) {
+  if (typeof dir == "function") {
+    cb = dir
+    dir = __dirname
+  }
 
-const noderedPath = path.join(__dirname, "..", "node-red", "red.js")
-fs.access(noderedPath, fs.constants.R_OK, (err) => {
-  if (err) {
-    if (err.code === "ENOENT") {
-      debug("Node red not Found")
-      var gulpFile = require('./gulpfile.js')
-      var gulp = require('gulp')
-      debug("Starting downloading...")
-      gulp.start('default', function (d, e) {
-        debug("done");
-        debug(d);
-        debug(e);
-      })
+  const noderedPath = path.join(dir, "..", "node-red", "red.js")
+  debug("Searching for node red")
+  fs.access(noderedPath, fs.constants.R_OK, (err) => {
+    debug("Search complete")
+    if (err) {
+      if (err.code === "ENOENT") {
+        debug("Node red not Found")
+        var gulpFile = require('./gulpfile.js')
+        var gulp = require('gulp')
+        debug("Starting downloading...")
+        gulp.start('default', function (d, e) {
+          debug("done");
+          debug(d);
+          debug(e);
+          cb(require(noderedPath))
+        })
+      }
     } else {
       debug("already found");
+      cb(require(noderedPath))
     }
-  }
-})
+  })
+}
 
-module.exports = noderedPath
+if (require.main === module) {
+  getRED(function (r) {
+    debug(r)
+  })
+} else {
+  module.exports = {
+    getRED: getRED
+  }
+}
